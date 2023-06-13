fe4a80604ebd3dfb3d9fbfd80be206926b4bb733
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-06-13 15:23:29 -0400

    Deploy - Added stuff to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "Portfolio - Joel Samaniego"; // Set your desired title here
+  }, []);
   return (
     <div>
       <Navbar />
--- src/data/data.js
+++ src/data/data.js
@@ -1,37 +1,21 @@
-import Prueba1 from "../assets/imagenprueba1.jpg";
-import Prueba2 from "../assets/imagenprueba2.jpg";
 import Jstodo from "../assets/jstodoapp.png";
+import ReactStore from "../assets/littlestore.png";
 
 export const data = [
   {
     id: 1,
-    name: "React JS Application",
-    image: Prueba1,
+    name: "Little React JS Application",
+    image: ReactStore,
     github:
-      "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/About.jsx",
-    live: "",
+      "https://github.com/samajoel/littlereactapp/tree/MFPR-MFPR-148-React-Store-Iteration-2/my-app/my-app",
+    live: "https://littlereactapp.vercel.app/",
   },
   {
     id: 2,
-    name: "React JS Application",
-    image: Prueba2,
-    github:
-      "https://github.com/fireclint/portfolio-app-react/blob/main/src/components/About.jsx",
-    live: "",
-  },
-  {
-    id: 3,
     name: "Little ToDo JS Application",
     image: Jstodo,
     github:
       "https://github.com/samajoel/samajoel.github.io/blob/main/script.js",
     live: "https://samajoel.github.io/",
-  },
-  {
-    id: 4,
-    name: "React JS Application",
-    image: Prueba2,
-    github: "",
-    live: "",
   },
 ];
