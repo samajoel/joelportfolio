ee8a417533b14d02181bd92f7b743f5882406a9e
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-06-26 19:16:31 -0400

    Add-New Project

--- src/data/data.js
+++ src/data/data.js
@@ -6,12 +6,19 @@ export const data = [
     id: 1,
     name: "Little React JS Application",
     image: ReactStore,
+    github: "https://github.com/samajoel/CNConstrucciones/tree/main/src",
+    live: "https://cn-construcciones.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Little React JS Application",
+    image: ReactStore,
     github:
       "https://github.com/samajoel/littlereactapp/tree/MFPR-MFPR-148-React-Store-Iteration-2/my-app/my-app",
     live: "https://littlereactapp.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Little ToDo JS Application",
     image: Jstodo,
     github:
